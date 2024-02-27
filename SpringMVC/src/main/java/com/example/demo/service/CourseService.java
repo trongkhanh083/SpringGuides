26ea9633623b26e3ae619c814d0c11491e50b289
trongkhanh083 <80797508+trongkhanh083@users.noreply.github.com>
2024-02-27 17:53:14 +0700

    Add files via upload

---
+++ SpringMVC/src/main/java/com/example/demo/service/CourseService.java
@@ -0,0 +1,15 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Course;
+import java.util.List;
+import org.springframework.data.domain.Page;
+
+public interface CourseService {
+    List<Course> getAllCourses();
+    void saveCourse(Course course);
+    Course getCourseById(long id);
+    void deleteCourseById(long id);
+    Page<Course> findPaginated(int pageNum, int pageSize,
+                               String sortField,
+                               String sortDirection);
+}
